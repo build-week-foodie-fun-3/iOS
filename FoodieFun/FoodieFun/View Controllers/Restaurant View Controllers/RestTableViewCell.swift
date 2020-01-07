0c63dd885a451b88425ecd826ccd330dfdd8d204
Dennis Rudolph <Dennis@Denniss-MacBook-Pro.local>
2020-01-06 20:20:16 -0500

    Storyboard Setup

---
+++ FoodieFun/FoodieFun/View Controllers/Restaurant View Controllers/RestTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestTableViewCell.swift
+//  FoodieFun
+//
+//  Created by Dennis Rudolph on 1/6/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class RestTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FoodieFun/FoodieFun/View Controllers/Review View Controllers/ReviewDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ReviewDetailViewController.swift
+//  FoodieFun
+//
+//  Created by Dennis Rudolph on 1/6/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ReviewDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
